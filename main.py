b2382dc6846facc76d2edc2453e492149ee40c04
tusharharyana <123148711+tusharharyana@users.noreply.github.com>
2024-08-10 10:02:12 +0530

    Starting of game created

---
+++ main.py
@@ -0,0 +1,14 @@
+#Snake'S Den Game
+
+from turtle import Turtle, Screen
+#Turtle library for graphics
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("Snake's Den")
+
+
+
+
+screen.exitonclick()
